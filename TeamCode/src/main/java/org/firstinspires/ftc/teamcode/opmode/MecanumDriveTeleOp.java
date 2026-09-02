736cedb6dce6235dd5ec40904ecfb3d07bd77bdb
jonathanqi-pixel <jonathan.qi@circuitrunners.com>
2026-09-02 13:39:30 -0400

    Update MecanumDriveTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MecanumDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MecanumDriveTeleOp.java
@@ -2,6 +2,7 @@
 
 package org.firstinspires.ftc.teamcode.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -10,55 +11,55 @@ import org.firstinspires.ftc.teamcode.Config.MecanumDrive;
 
 //@Disabled
 @TeleOp(name = "Mecanum -Robot Centric")
-public class MecanumDriveTeleOp extends OpMode {
+public class MecanumDriveTeleOp extends LinearOpMode {
     private MecanumDrive drive;
 
     private double speedMultiply = 0.5;
 
     @Override
-    public void init(){
+    public void runOpMode() {
         telemetry.addLine("Initializing...");
         telemetry.update();
-
-
 
         drive = new MecanumDrive();
         drive.init(hardwareMap);
 
         telemetry.addLine("Ready!");
         telemetry.update();
-    }
 
-    @Override
-    public void loop() {
+        waitForStart();
+
+        while(opModeIsActive()){
 
 
-        if (gamepad1.dpad_up) {
-            speedMultiply = 0.25;
-        } else if (gamepad1.dpad_left) {
-            speedMultiply = 0.5;
-        } else if (gamepad1.dpad_down) {
-            speedMultiply = 0.75;
-        } else if (gamepad1.dpad_right)  {
-            speedMultiply = 1.0;
+            if (gamepad1.dpad_up) {
+                speedMultiply = 0.25;
+            } else if (gamepad1.dpad_left) {
+                speedMultiply = 0.5;
+            } else if (gamepad1.dpad_down) {
+                speedMultiply = 0.75;
+            } else if (gamepad1.dpad_right)  {;
+
+                speedMultiply = 1.0;
+            }
+
+            double forward = -1*gamepad1.left_stick_y * speedMultiply;
+            double strafe  =  gamepad1.left_stick_x * speedMultiply;
+            double rotate  =  gamepad1.right_stick_x * speedMultiply;
+            /** Send inputs to drive class using method created in Mecanum Drive Class */
+            drive.drive(forward, strafe, rotate);
+
+            telemetry.addData("fl power", drive.frontLeftMotor.getPower());
+            telemetry.addData("fr Power", drive.frontRightMotor.getPower());
+            telemetry.addData("rl Power", drive.backLeftMotor.getPower());
+            telemetry.addData("rr Power", drive.backRightMotor.getPower());
+
+            telemetry.addLine("sloth");
+
+
+
+            telemetry.update();
         }
-
-        double forward = -1*gamepad1.left_stick_y * speedMultiply;
-        double strafe  =  gamepad1.left_stick_x * speedMultiply;
-        double rotate  =  gamepad1.right_stick_x * speedMultiply;
-
-        /** Send inputs to drive class using method created in Mecanum Drive Class */
-        drive.drive(forward, strafe, rotate);
-
-        telemetry.addData("fl power", drive.frontLeftMotor.getPower());
-        telemetry.addData("fr Power", drive.frontRightMotor.getPower());
-        telemetry.addData("rl Power", drive.backLeftMotor.getPower());
-        telemetry.addData("rr Power", drive.backRightMotor.getPower());
-
-        telemetry.addLine("sloth");
-
-
-
-        telemetry.update();
     }
+
 }
